f0c5ba094ea295a239ad4635e0fe6fbc75326748
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-09-09 12:15:07 +0200

    blackened code

--- citesting/citesting.py
+++ citesting/citesting.py
@@ -1,5 +1,6 @@
 def f(x):
     return x + x**2
 
+
 def f2(x):
     return x**2
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -3,23 +3,23 @@ import pytest
 
 
 def test_ci_f_0():
-    '''testing citesting.citesting.f(x) function'''
+    """testing citesting.citesting.f(x) function"""
     assert ci.f(0) == 0
 
 
 def test_ci_f_1():
-    '''testing citesting.citesting.f(x) function'''
+    """testing citesting.citesting.f(x) function"""
     assert ci.f(1) == 2
     assert ci.f(2) == 6
 
 
 def test_ci_f2_0():
-    '''testing citesting.citesting.f2(x) function'''
+    """testing citesting.citesting.f2(x) function"""
     assert ci.f2(0) == 0
     assert ci.f2(2) == 4
 
 
 @pytest.mark.xfail(raises=AssertionError)
 def test_ci_f2_1():
-    '''testing citesting.citesting.f2(x) function'''
+    """testing citesting.citesting.f2(x) function"""
     assert ci.f2(3) != 10  # raises AssertionError
